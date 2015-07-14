5b353bab73deeab597dc8897c8cd1b6b2222aaa7
Tan Kucukhas <tkucukhas@sscg109175.sscg.local>
2015-07-14 13:40:08 -0400

    create angular-bootstrap-sass build

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name agfv2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the agfv2App
+ */
+angular.module('agfv2App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name agfv2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the agfv2App
+ */
+angular.module('agfv2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
